f07ea394f6097d7a417e3c63923831aae86885a7
Vitaliy Krasylovets <Vitaliy@Vitaliys-MacBook-Pro.local>
2014-11-15 15:43:22 -0500

    Added the .Obj class

---
+++ TankSim/ObjMesh.h
@@ -0,0 +1,62 @@
+#ifndef __CityBuilder2__ObjMesh__
+#define __CityBuilder2__ObjMesh__
+
+#ifdef __APPLE__
+    #include <OpenGL/glu.h>
+    #include <OpenGL/glu.h>
+#else
+    #include <windows.h>
+    #include <gl/gl.h>
+    #include <gl/glu.h>
+#endif
+
+#include <cmath>
+#include <stdio.h>
+#include <vector>
+
+class Point3D {
+    public:
+    
+    //Variables
+    GLfloat x,y,z;
+    
+    //functions
+    Point3D(void) : x(0.0), y(0.0), z(0.0){};
+    Point3D(GLfloat x, GLfloat y, GLfloat z) : x(x), y(y), z(z) {};
+};
+
+class ObjMesh {
+public:
+    Point3D *vertices = NULL;
+    Point3D *normals = NULL;
+    GLuint *indices = NULL; // unsigned integer
+    
+    int num_of_vertices;
+    int num_of_normals;
+    int num_of_indices;
+    
+    //Transformations 
+    float tx, ty, tz;			// Translatation Deltas
+    float sfx, sfy, sfz;		// Scale Factors
+    float angle;
+    
+    float mat_ambient[4];
+    float mat_specular[4];
+    float mat_diffuse[4];
+    float mat_shininess[1];
+    
+    bool immediate_render = true;
+    
+    //Texturing
+    int textureID = -1;
+    
+    //Functions
+    ObjMesh (std::vector<Point3D> &vertices, std::vector<Point3D> &normals,std::vector<GLuint> &indices);
+    void draw ();
+    void setTextureMapID (int textureID);
+};
+
+
+void load_obj (std::string filename, ObjMesh **mesh);
+
+#endif /* defined(__CityBuilder2__ObjMesh__) */
